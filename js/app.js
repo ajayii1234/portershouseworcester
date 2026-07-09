ccf66a3c7fb3aba9f7fddc76fd66bfab8eab9254
ajayii1234 <ajayi.femi.eee@gmail.com>
2026-07-09 19:55:30 +0100

    3rd change

--- js/app.js
+++ js/app.js
@@ -402,3 +402,18 @@ navItems.forEach(link=>{
     }
 
 });
+
+
+// hamburger
+
+const hamburger = document.querySelector(".hamburger");
+
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", function(){
+
+    navLinks.classList.toggle("active");
+
+});
+
+
